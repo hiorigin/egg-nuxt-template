080d7b50a0ee9c7d46c60c2674c2a9ba8ea84982
samhanx <xuanxinhan@vip.qq.com>
2018-09-26 23:37:40 +0800

    config: init config

---
+++ config/config.default.js
@@ -0,0 +1,4 @@
+module.exports = {
+  keys: 'EGG_NUXT_APP',
+  middleware: ['nuxtRender'],
+}
---
+++ nuxt.config.js
@@ -0,0 +1,19 @@
+const pkg = require('./package')
+
+module.exports = {
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'renderer', content: 'webkit' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no, viewport-fit=cover' },
+      { name: 'X-UA-Compatible', content: 'IE=edge' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  srcDir: './nuxt/',
+  loading: { color: '#3b8070' },
+}
