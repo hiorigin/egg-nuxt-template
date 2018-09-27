89c7c4186158218b3f1b64c05e3f128e8935742b
Hanx <xuanxinhan@vip.qq.com>
2018-09-27 14:02:22 +0800

    style: eslint fix

--- app/middleware/nuxt_render.js
+++ app/middleware/nuxt_render.js
@@ -4,7 +4,7 @@ module.exports = (options, app) => {
 
     const isApi = app.router.stack.some(el => ctx.path === el.path)
     if (isApi) return
-    
+
     const { res, req, body } = ctx
     ctx.status = 200
     res.data = body
