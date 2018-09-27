890248f06a297cabda28d54a98c670e6f40ea5dd
Hanx <xuanxinhan@vip.qq.com>
2018-09-27 13:58:39 +0800

    style: eslint fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,11 +8,11 @@ module.exports = {
       { name: 'renderer', content: 'webkit' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no, viewport-fit=cover' },
       { name: 'X-UA-Compatible', content: 'IE=edge' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: pkg.description },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    ],
   },
   modules: [
     ['@nuxtjs/axios', { browserBaseURL: '/', port: 7001 }],
