a291833135f18d4355d6317224f16315a7466cb0
Hanx <xuanxinhan@vip.qq.com>
2018-09-27 09:58:42 +0800

    feat: use async data

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -3,14 +3,17 @@
     <h1 class="title">
       Universal Vue.js Application Framework
     </h1>
+    <ul>
+      <li v-for="item in data" :key="item.id">{{ item.title }}</li>
+    </ul>
   </section>
 </template>
 
 <script>
 export default {
-  async mounted() {
-    let data = await this.$axios.$get(`/api/topics`)
-    console.log(data)
-  }
+  async asyncData({ app }) {
+    let { data } = await app.$axios.$get(`/api/topics`)
+    return { data }
+  },
 }
 </script>
