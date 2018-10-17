19ac75ece96fb7bef18d343704e312ef968c63af
samhanx <xuanxinhan@vip.qq.com>
2018-10-17 23:54:56 +0800

    style: no-console comment

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 module.exports = app => {
   const isDev = app.config.env === 'local'
 
@@ -5,10 +6,10 @@ module.exports = app => {
     const { Builder } = require('nuxt')
     const nuxtBuilder = new Builder(app.nuxt)
     nuxtBuilder.build().then(() => {
-      console.log('[nuxt] Build Success.')        // eslint-disable-line no-console
+      console.log('[nuxt] Build Success.')
     }).catch(err => {
-      console.log('[nuxt] Building Error:')       // eslint-disable-line no-console
-      console.log(err)                            // eslint-disable-line no-console
+      console.log('[nuxt] Building Error:')
+      console.log(err)
       process.exit(1)
     })
   }
