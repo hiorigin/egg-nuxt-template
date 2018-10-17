04017c5e8aa275c903eb765951bf8d3e052c9122
samhanx <xuanxinhan@vip.qq.com>
2018-10-17 23:30:29 +0800

    fix: nuxt not render when starting as an egg app

--- app/extend/application.js
+++ app/extend/application.js
@@ -1,11 +1,12 @@
 const { Nuxt } = require('nuxt')
 const NUXT = Symbol('APP#nuxt')
 const config = require(`${process.cwd()}/nuxt.config.js`)
+const nuxt = new Nuxt(config)
 
 module.exports = {
   get nuxt() {
     if (!this[NUXT]) {
-      this[NUXT] = new Nuxt(config)
+      this[NUXT] = nuxt
     }
     return this[NUXT]
   },
