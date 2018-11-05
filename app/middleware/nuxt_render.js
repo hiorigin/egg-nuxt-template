9fcab9ff042525e990e47bde608fe584163402e9
Hanx <xuanxinhan@vip.qq.com>
2018-11-05 23:21:16 +0800

    refactor: clean code

--- app/middleware/nuxt_render.js
+++ app/middleware/nuxt_render.js
@@ -1,13 +1,10 @@
 module.exports = (options, app) => {
   return async (ctx, next) => {
     await next()
-
-    const isApi = app.router.stack.some(el => ctx.path === el.path)
-    if (isApi) return
-
-    const { res, req, body } = ctx
+    if (app.router.stack.some(el => ctx.path === el.path)) return
+    const { res, req } = ctx
     ctx.status = 200
-    res.data = body
+
     return new Promise((resolve, reject) => {
       res.on('close', resolve)
       res.on('finish', resolve)
